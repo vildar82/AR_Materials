2c8f7fc90d482461b77ac57e53fb769110e790f7
Vildar Khisyametdinov <vildar82@gmail.com>
2016-10-19 16:21:16 +0300

    command start

--- AR_Interior/Commands.cs
+++ AR_Interior/Commands.cs
@@ -12,36 +12,31 @@ namespace AR_Materials
 {
     public class Commands
     {
-        [CommandMethod("PIK", "ARM-Help", CommandFlags.Modal | CommandFlags.NoPaperSpace | CommandFlags.NoBlockEditor)]
-        public void ARMHelpCommand()
+        [CommandMethod("PIK", nameof(ARMHelp), CommandFlags.Modal | CommandFlags.NoPaperSpace | CommandFlags.NoBlockEditor)]
+        public void ARMHelp()
         {
-            Document doc = Application.DocumentManager.MdiActiveDocument;
-            if (doc == null) return;
+            AcadLib.CommandStart.Start((doc) =>
+            {
+                StringBuilder msgHelp = new StringBuilder();
+                msgHelp.AppendLine("ARM-MaterialsCalc - Программа расчета материалов помещений по выбранным блокам.");
+                msgHelp.Append("Версия ");
+                msgHelp.Append(Assembly.GetExecutingAssembly().GetName().Version);
+                msgHelp.AppendLine();
+                msgHelp.AppendLine("Блоки для расчета:");
+                msgHelp.AppendLine("АРМ_Помещение - динамический блок помещения. Для сложного в плане помещения выполнить контестное редактирование этого блока.");
+                msgHelp.AppendLine("АРМ_Проём - блок для проемов. Для дверного проема располагать этот блок на слое АР_Двери, для оконного - АР_Окна. Блок должен пересекать полилинию нужного блока помещения.");
+                msgHelp.AppendLine("АРМ_Добавка - блок добавки к материалу. Величина добавки определяется по ширине и длине этого блока. Значение атрибутов: Принадлежность - стены, пол, потолок, плинтус, карниз; Материал - имя материала добавки, если пусто, то считается материал принадлежащей конструкцтии из первого атрибута; Действие - +, -, или +- (отнять из материала принадлежащей конструкции и добавить указанный материал в добавке). Точка вставки блока должна быть внутри/на полилинии нужного блока помещения.");
+                msgHelp.AppendLine("АРМ_Унитаз - блок для учета короба над унитазом. Атрибут примыкания: 1 - если короб примыкает, только длинной стороной к стене; 2 - если длинной и короткой. Точка вставки блока должна быть внутри/на полилинии нужного блока помещения.");
 
-            StringBuilder msgHelp = new StringBuilder();
-            msgHelp.AppendLine("ARM-MaterialsCalc - Программа расчета материалов помещений по выбранным блокам.");
-            msgHelp.Append("Версия ");
-            msgHelp.Append(Assembly.GetExecutingAssembly().GetName().Version);
-            msgHelp.AppendLine();
-            msgHelp.AppendLine("Блоки для расчета:");
-            msgHelp.AppendLine("АРМ_Помещение - динамический блок помещения. Для сложного в плане помещения выполнить контестное редактирование этого блока.");
-            msgHelp.AppendLine("АРМ_Проём - блок для проемов. Для дверного проема располагать этот блок на слое АР_Двери, для оконного - АР_Окна. Блок должен пересекать полилинию нужного блока помещения.");
-            msgHelp.AppendLine("АРМ_Добавка - блок добавки к материалу. Величина добавки определяется по ширине и длине этого блока. Значение атрибутов: Принадлежность - стены, пол, потолок, плинтус, карниз; Материал - имя материала добавки, если пусто, то считается материал принадлежащей конструкцтии из первого атрибута; Действие - +, -, или +- (отнять из материала принадлежащей конструкции и добавить указанный материал в добавке). Точка вставки блока должна быть внутри/на полилинии нужного блока помещения.");
-            msgHelp.AppendLine("АРМ_Унитаз - блок для учета короба над унитазом. Атрибут примыкания: 1 - если короб примыкает, только длинной стороной к стене; 2 - если длинной и короткой. Точка вставки блока должна быть внутри/на полилинии нужного блока помещения.");
-
-            doc.Editor.WriteMessage("\n{0}", msgHelp);
+                doc.Editor.WriteMessage("\n{0}", msgHelp);
+            });
         }
 
         [CommandMethod("PIK", "ARM-MaterialsCalc", CommandFlags.Modal | CommandFlags.NoPaperSpace | CommandFlags.NoBlockEditor)]
         public void ARMMaterialsCalcCommand()
         {
-            try
+            AcadLib.CommandStart.Start((doc) =>
             {
-                AutoCAD_PIK_Manager.Log.Info("Plugin AR_Materials Start Command MaterialsCalc");
-
-                Inspector.Clear();
-                Counter.Clear();
-
                 // Поиск всех блоков помещений и проемов в чертеже.
                 Model.Materials.RoomService roomService = new Model.Materials.RoomService();
                 roomService.FindAllBlocks();
@@ -70,15 +65,7 @@ namespace AR_Materials
                 // Построение таблицы.
                 Model.Materials.Result.ToTable toTable = new Model.Materials.Result.ToTable(roomService.Rooms);
                 toTable.Insert();
-
-                // Експорт в ексель.            
-            }
-            catch (System.Exception ex)
-            {
-                Document doc = Application.DocumentManager.MdiActiveDocument;
-                doc.Editor.WriteMessage("\n{0}", ex.ToString());
-                AutoCAD_PIK_Manager.Log.Error(ex, "MaterialsCalc");
-            }
+            });
         }
 
         /// <summary>
@@ -87,23 +74,10 @@ namespace AR_Materials
         [CommandMethod("PIK", "AI-RollUp", CommandFlags.Modal | CommandFlags.NoPaperSpace | CommandFlags.NoBlockEditor)]
         public void ARI_RollUp()
         {
-            Logger.Log.Info("Start command AI-RollUp");
-            Document doc = Application.DocumentManager.MdiActiveDocument;
-            if (doc == null) return;            
-            try
+            AcadLib.CommandStart.Start((doc) =>
             {
-                Inspector.Clear();
                 Model.Interiors.RollUpService.CreateRollUp();
-                Inspector.Show();
-            }
-            catch (System.Exception ex)
-            {
-                doc.Editor.WriteMessage($"\nОшибка : {ex.Message}");
-                if (!ex.Message.Contains("Отменено пользователем"))
-                {
-                    Logger.Log.Error(ex, $"Command: AI-RollUp. {doc.Name}");
-                }
-            }
+            });            
         }
     }
 }
